f40e4ba4a2791ecf182c322b12edc7d2274dd1eb
WGzeyu <785777793@qq.com>
2019-09-21 10:21:37 +0800

    降低IP获取速度及临时取消获取更新的同时提供IP
    由于WS分布式部署服务器新增美国节点，各节点之间延迟最高能到130ms，刚加入房间时发送网络信息的速度比这个更快就可能发不出去，临时降低发送网络信息的速度来临时解决这个问题

--- php/updatecheck.php
+++ php/updatecheck.php
@@ -17,7 +17,9 @@ for ($yicishu=$nowver+1; $yicishu<=$newver; $yicishu++) {
 	}
 } 
 $moreinfo = getSubstr($file,"<serverinfo>","</serverinfo>");
-$txtecho = $txtecho.="<serverinfo>".$moreinfo."<nowip>".$fromip."</nowip>"."</serverinfo>";
+//$txtecho = $txtecho.="<serverinfo>".$moreinfo."<nowip>".$fromip."</nowip>"."</serverinfo>";
+// 似乎由于分布式部署各服务器间延迟大于用户刚加入房间后第一次发送网络信息的时间，可能导致发不出去用户的网络信息，服务器暂时不再提供IP，降低网络信息获取速度来临时解决此问题
+$txtecho = $txtecho.="<serverinfo>".$moreinfo."</serverinfo>";
 echo("<base64>".base64_encode($txtecho)."</base64>");
 
 function getSubstr($str, $leftStr, $rightStr)
